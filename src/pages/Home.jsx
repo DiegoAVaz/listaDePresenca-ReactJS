19856a9bf5774fdf35e7e28954c2fef0cce52130
Diego Alves <diegoalves32@gmail.com>
2022-11-16 16:13:31 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <> 
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
+    </>
+  )
+}
+
+export default Home
+
+// Podemos usar div em vez do <></> para empacotar os elementos
